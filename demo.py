17c8f3bb75f62ebb760cde35e4536be90172e40f
BEAK5500 <b.munseon100@gmail.com>
2023-03-03 13:58:46 +0900

    save

--- demo.py
+++ demo.py
@@ -1,7 +1,15 @@
 import numpy
+from sympy import symbols, solve
 
-print('sda')
-print(1+2)
-new = numpy.array([[123],[455],[678]])
+#04.28=8.03+a+b
+#-9.54=-1.4*8.03-1.1*b+0.2
+#b?a?
 
-print(new)
+#x=Symbol('x')
+#equation = 2 * x - 6
+#print(solve(equation))
+
+o, b =symbols('o,b')
+equation1 = 8.03 + o + b-4.28
+equation2 = (-1.4*8.03)-(1.1*b)+0.2+9.54
+print(solve((equation2,equation1)))
